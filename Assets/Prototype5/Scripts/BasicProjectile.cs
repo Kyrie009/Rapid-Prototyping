995930e09976f91984cc702b3a8ed2aaec00b485
dbui <dbui1@myjmc.edu.au>
2022-04-26 17:18:24 +1000

    8.1

--- Assets/Prototype5/Scripts/BasicProjectile.cs
+++ Assets/Prototype5/Scripts/BasicProjectile.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class BasicProjectile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private int atk = 100;
+    //[SerializeField] private Transform hitVFX;
+    private Rigidbody bulletRigidbody;
+
+    private void Awake()
     {
-        
+        bulletRigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        bulletRigidbody.velocity = transform.forward * speed; //bullet movement
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<EnemyAI4>() != null)
+        {
+            other.GetComponent<EnemyAI4>().Hit(atk);
+        }
+        //Instantiate(hitVFX, transform.position, Quaternion.identity);
+        Destroy(gameObject, 1f);
     }
 }
